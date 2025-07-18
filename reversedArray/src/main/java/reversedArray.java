8aec16c8c67e68d1825edc6fff8ffb8dc764961d
ChrisEmma65 <chrisemmanuel65@gmail.com>
2025-07-18 06:52:19 +0100

    Add files via upload

---
+++ reversedArray/src/main/java/reversedArray.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class reversedArray {
+    public static void main(String[] args) {
+int[] numbers = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+
+Arrays.sort(numbers);
+        System.out.println("Numbers in ascending order: " + Arrays.toString(numbers));
+    }
+}
